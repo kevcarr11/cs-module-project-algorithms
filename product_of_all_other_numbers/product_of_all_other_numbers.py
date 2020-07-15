cbe052b6a2d7eabf5f25a9313ee4210a118068d7
Kevin <kevincarr632@gmail.com>
2020-07-14 20:53:52 -0500

    completed problems 1-3

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -4,13 +4,32 @@ Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
     # Your code here
+    n = len(arr)
+    left = [0] * n
+    right = [0] * n
 
-    pass
+    product_arr = [0] * n
+    left[0] = 1
+    right[n - 1] = 1
+    # Construct the left array 
+    for i in range(1, n):
+        left[i] = arr[i - 1] * left[i - 1]
+    # Construct the right array  
+    for j in range(n-2, -1, -1):  
+        right[j] = arr[j + 1] * right[j + 1]  
+  
+    # Construct the product array using  
+    # left[] and right[]  
+    for i in range(n):  
+        product_arr[i] = left[i] * right[i]  
+
+    return product_arr
+        
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 2, 3, 4, 5]
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
